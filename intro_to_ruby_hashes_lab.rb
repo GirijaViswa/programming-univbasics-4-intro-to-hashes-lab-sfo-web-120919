9ef84759afae12bd87b36ecd95425c31ab1e4ed6
GIRIJA VISWANATHAN <girijavizhwa@gmail.com>
2019-12-07 01:02:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,12 +10,12 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  my_hash = {:name => "Grace Hopper"}
+  my_hash = {:name => 'Grace Hopper'}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
-  my_hash = {:name => "Grace Hopper", :id => 5}
+  my_hash = {:name => 'Grace Hopper', :id => 5}
 end
 
 def my_hash_creator(key, value)
